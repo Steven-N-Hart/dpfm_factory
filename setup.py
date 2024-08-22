be2883c568d857e95e8bdc29ac4a7edeff80a8da
Hart.Steven <hart.steven@mayo.edu>
2024-08-22 09:05:47 -0500

    added README

--- setup.py
+++ setup.py
@@ -15,6 +15,7 @@ setup(
     author_email="Hart.Steven@Mayo.edu",
     description="Helper scripts for digital pathology foundation models",
     long_description=open('README.md').read(),  # Make sure you have a README.md file
+    long_description_content_type="text/markdown",  # Specify that README is in Markdown
     packages=find_packages(),
     install_requires=parse_requirements('requirements.txt'),
     dependency_links=parse_dependency_links('requirements.txt'),
