ffe21027d15282c5b9591b13c7c5e0d4809fae58
Hart.Steven <hart.steven@mayo.edu>
2024-08-22 07:40:47 -0500

    updated conch install

--- setup.py
+++ setup.py
@@ -1,14 +1,19 @@
 from setuptools import setup, find_packages
 
-# Function to read the requirements from the requirements.txt file
 def parse_requirements(filename):
     with open(filename, 'r') as file:
-        return file.read().splitlines()
+        return [line.strip() for line in file if not line.startswith("git+")]
+
+def parse_dependency_links(filename):
+    with open(filename, 'r') as file:
+        return [line.strip() for line in file if line.startswith("git+")]
 
 setup(
     name="dpfm_factory",
     version="0.1",
     packages=find_packages(),
     install_requires=parse_requirements('requirements.txt'),
+    dependency_links=parse_dependency_links('requirements.txt'),
     package_dir={'': 'dpfm_model_runners'}
+
 )
